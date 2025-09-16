714f3a41c5d9021ac351a3d4cd36079ae63f93ac
Farhan Hossain <yourlocalfifa@gmail.com>
2025-09-16 20:17:27 +0000

    lesson

--- part2/lesson/src/App.jsx
+++ part2/lesson/src/App.jsx
@@ -1,6 +1,35 @@
 import Note from './Note'
+import { useState } from 'react'
 
-const App = ({notes}) => {
+
+const App = (props) => {
+  const [notes, setNotes ] = useState(props.notes)
+  const [newNotes, setNewNotes] = useState(
+    'a new note...'
+  )
+  const [showAll, setShowAll] = useState(true)
+  const notesToShow = showAll 
+    ? notes 
+    : notes.filter(note => note.important)
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObj = {
+      content: newNotes,
+      important: Math.random() < 0.5,
+      id: String(notes.length + 1)
+    }
+
+    setNotes(notes.concat(noteObj))
+    setNewNotes('')
+  }
+  
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNotes(event.target.value)
+  }
+
+
   return (
     <div>
       <h1>Notes</h1>
@@ -9,6 +38,13 @@ const App = ({notes}) => {
           <Note key={note.id} note={note} />
         )}
       </ul>
+      <button onClick={() => setShowAll(!showAll)}>
+        show {showAll ? 'important' : 'all' }
+      </button>
+      <form onSubmit={addNote}>
+        <input value={newNotes} onChange = {handleNoteChange} />
+        <button type='submit'>save</button>
+      </form>
     </div>
   )
 }
